879bc85f325efe41d2cd8209dc2cad49cf6add81
Anita <anitaghanata@gmail.com>
2023-04-11 20:43:00 +0530

    Instantiate RWonly class

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] arg){
         RWOnly obj=new RWOnly();
 
-        obj.setName("Arsalan");
+        obj.setName("Anita");
         String ans= obj.getName();
 
         System.out.println(ans);
